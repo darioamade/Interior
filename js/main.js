409bc437931f30d95130be58aa758415f3273686
darioamade <dario_isd@outlook.com>
2020-07-07 10:17:04 +0100

    commit
    updated js

--- js/main.js
+++ js/main.js
@@ -1,10 +1,44 @@
-const menuOpen = document.querySelector('.menu-open');
-const menuClose = document.querySelector('.menu-close');
-const nav = document.querySelector('.nav');
+const menuOpen = document.querySelector(".menu-open");
+const menuClose = document.querySelector(".menu-close");
+const nav = document.querySelector(".nav");
 
-menuOpen.addEventListener('click', menuTog);
-menuClose.addEventListener('click', menuTog);
+menuOpen.addEventListener("click", menuTog);
+menuClose.addEventListener("click", menuTog);
 
-function menuTog(e){
-  nav.classList.toggle('active')
-};
+function menuTog(e) {
+  nav.classList.toggle("active");
+}
+
+
+
+const sendBtn = document.querySelector('#sendBtn');
+const messageIn = document.querySelector('#messageIn');
+const messageOut = document.querySelector('#messageOut');
+
+
+sendBtn.addEventListener("click", sendMsg)
+
+  function sendMsg(){
+    let content = messageIn.value
+    //console.log(content);
+    if(content === ''){
+      alert('Please enter Valid value')
+    }
+    else {
+      messageOut.innerHTML = `Message sent!`;
+      messageIn.value = '';
+    }
+   
+  }
+
+
+  gsap.from(".hero-content", {
+    duration: 1,
+    x: "-100%",
+    ease: "bounce",
+    delay: 0.5,
+  });
+
+
+
+
